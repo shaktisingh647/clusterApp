150bb1a738ec20d6285fc5535662e5556d4c22f4
Shakti Singh <consolelogshakti@gmail.com>
2025-10-21 09:17:28 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ export const Home = () => {
     <div className="bg-black ">
       
       <Navbar />
+      <h1>shakti</h1>
       <div className="max-w-6xl mx-auto p-3 md:flex items-center justify-around gap-10 ">
         <div>
             <h1 className="text-3xl font-bold text-white">Explore the World, One <br />Country at a Time </h1>
