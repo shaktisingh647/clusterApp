a17ba3a8658f1315f01aad70cd9f66ffd0b247be
Shakti Singh <consolelogshakti@gmail.com>
2025-10-21 16:19:22 +0530

    counter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import world from "../assets/world.png";
 import { Card } from "../components/Card";
 import apiData from "../data/apiData.json"
+
 export const Home = () => {
+  const [count,setCount] = useState(0);
+  const addHandler = () =>{
+    setCount(count+1);
+  }
+
+  const substractHandler = () =>{
+    setCount(count-1);
+  }
   return (
-    <div className="bg-black ">
+    <div className="bg-white">
       
       <Navbar />
-      
+      <h1>creating counter</h1>
+      <button onClick={addHandler}>+</button>
+      <span>{count}</span>
+      <button onClick={substractHandler}>-</button>
       <div className="max-w-6xl mx-auto p-3 md:flex items-center justify-around gap-10 ">
         <div>
             <h1 className="text-3xl font-bold text-white">Explore the World, One <br />Country at a Time </h1>
